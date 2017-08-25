1bf7a709cb5ed7b13f5c248316a6ef099e4ad5cd
Luca Stefani <luca020400@lineageos.org>
2017-08-25 21:13:59 +0200

    Add HIDL manifest
    Change-Id: I890b944817d969b7604c8f3cf1b55cca449c9c40

--- device.mk
+++ device.mk
@@ -155,6 +155,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf \
     $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/etc/xtwifi.conf
 
+
+# HIDL
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
+
 # Graphics
 PRODUCT_PACKAGES += \
     libsdm-disp-apis
