22cbfe5ee6b4aea7906a7ea1686b951aeb01ed5e
Martinusbe <martinusbe@gmail.com>
2017-08-30 16:56:15 +0200

    add camera hal to manifest
    Change-Id: Ie7192d3813f2988772b583a1f4599eca81d6f368

--- device.mk
+++ device.mk
@@ -123,9 +123,9 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    Snap \
     camera.device@3.2-impl \
-    android.hardware.camera.provider@2.4-impl \
-    Snap
+    android.hardware.camera.provider@2.4-impl
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
