8b5267a2003627c28745d968f376d979df80f861
Martinusbe <martinusbe@gmail.com>
2017-08-30 15:58:58 +0200

    add renderscript hal
    Change-Id: I145c10fe6c87bd78cd9e72080b3fd3ab83a0fa77
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- device.mk
+++ device.mk
@@ -286,6 +286,10 @@ PRODUCT_PACKAGES := \
     android.hardware.radio@1.0-impl \
     android.hardware.radio.deprecated@1.0-impl
 
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # RIL
 PRODUCT_PACKAGES += \
     librmnetctl \
